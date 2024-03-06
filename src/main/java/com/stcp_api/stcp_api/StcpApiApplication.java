96021e796484176c69c60d746bcab923721262df
Sergio <1210891@isep.ipp.pt>
2024-03-06 21:36:33 +0000

    Initial project structure and dependencies

---
+++ src/main/java/com/stcp_api/stcp_api/StcpApiApplication.java
@@ -0,0 +1,13 @@
+package com.stcp_api.stcp_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StcpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StcpApiApplication.class, args);
+	}
+
+}
