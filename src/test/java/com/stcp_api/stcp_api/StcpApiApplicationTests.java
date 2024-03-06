c1adf2f5bf6d34ababf20c9bcb939af3f7642c87
Sergio <1210891@isep.ipp.pt>
2024-03-06 21:36:33 +0000

    Initial project structure and dependencies

---
+++ src/test/java/com/stcp_api/stcp_api/StcpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stcp_api.stcp_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StcpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
