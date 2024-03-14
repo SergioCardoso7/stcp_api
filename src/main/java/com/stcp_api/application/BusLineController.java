fad12821315abaf4ca30e9b2c34a16130cce1b81
Sergio <1210891@isep.ipp.pt>
2024-03-14 11:33:54 +0000

    implementation of getStopsOfLine api call and some code refactoring

--- src/main/java/com/stcp_api/application/BusLineController.java
+++ src/main/java/com/stcp_api/application/BusLineController.java
@@ -1,10 +1,12 @@
 package com.stcp_api.application;
 
-import com.stcp_api.domain.model.BusLine;
+import com.stcp_api.domain.model.BusLineDTO;
+import com.stcp_api.domain.model.BusStopDTO;
 import com.stcp_api.domain.services.BusLineService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,17 +15,21 @@ import java.util.List;
 @RequestMapping(path = "stcp_api/busline")
 public class BusLineController {
 
-
     private final BusLineService busLineService;
+
     @Autowired
-    public BusLineController(BusLineService service){
+    public BusLineController(BusLineService service) {
         this.busLineService = service;
     }
 
     @GetMapping("/getbuslines")
-    public List<BusLine> getAllBusLines() {
-
+    public List<BusLineDTO> getAllBusLines() {
         return busLineService.getAllBusLines();
+    }
 
+    @GetMapping("/linestops")
+    public List<BusStopDTO> getStopsOfLine(@RequestParam String lineCode,
+                                           @RequestParam int directionCode) {
+        return busLineService.getLineBusStops(lineCode, directionCode);
     }
 }
